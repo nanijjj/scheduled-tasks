dde0716d0779bb3706c8f9d235786df3b63f7bda
nanijjj <65719666+nanijjj@users.noreply.github.com>
2026-07-08 01:03:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,45 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
+from dotenv import load_dotenv
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+load_dotenv("C:/Users/PCUser/Documents/.env")
+api_key = os.getenv("rain_api_key")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+response = requests.get("https://api.openweathermap.org/data/2.5/forecast", params={"lat":54.036755, "lon":22.502927,"appid":api_key,"cnt":4})
+
+response.raise_for_status()
+data = response.json()
+
+weather_data = data["list"]
+
+def will_rain(id_codes:list):
+    for id_entry in id_codes:
+        if id_entry < 700:
+            return True
+    return False
+
+def telegram_bot_sendtext(bot_message):
+    bot_token = os.getenv("rain_bot_token")
+    bot_chatID = '8847528871'
+    send_text = f"https://api.telegram.org/bot{bot_token}/sendMessage"
+
+    response = requests.get(send_text,params={"parse_mode": "Markdown","chat_id":bot_chatID,"text":bot_message})
+
+    return response.json()
+
+list_of_codes = []
+for entry in weather_data:
+    desc = entry["weather"][0]
+    id_code = desc["id"]
+    list_of_codes.append(id_code)
+
+
+
+if will_rain(list_of_codes):
+    telegram_bot_sendtext("It's going to rain!")
+else:
+    telegram_bot_sendtext("It will not rain in the next 12 hours!")
+
+
